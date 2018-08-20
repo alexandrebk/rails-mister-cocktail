2bb8fa5ba6eaaa1f5eedbf7349b4f063346a0605
alexandrebk <alexandre.b2506@gmail.com>
2018-08-20 11:08:30 +0200

    refacto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,35 +7,13 @@ class ApplicationController < ActionController::Base
 
   # before_action :set_review, only: [:show, :edit, :update, :destroy]
 
+
   ACTIONS.each do |action|
     define_method("#{action}") do
-      name_class = self.class.to_s.split("sC")
-      var_instance = "@#{name_class[0].downcase}"
-      self.instance_variable_set( var_instance, name_class[0].constantize.find(params[:id]) )
+      class_name        = self.class.to_s.split("sC")
+      instance_variable = "@#{class_name[0].downcase}"
+      self.instance_variable_set( instance_variable, class_name[0].constantize.find(params[:id]) )
     end
   end
-
-  # def show
-  #   name_class = self.class.to_s.split("sC")
-  #   var_instance = "@#{name_class[0].downcase}"
-  #   self.instance_variable_set( var_instance, name_class[0].constantize.find(params[:id]) )
-  # end
-
-
-  # CLASSES.each do |toto|
-
-  #   puts "debug"
-
-  #   p toto
-
-  #   def index
-  #     @restaurants = toto.capitalize.constantize.all
-  #   end
-  #   define_method("set_#{toto}") do
-  #     @restaurant = toto.capitalize.constantize.find(params[:id])
-  #     # fred.instance_variable_set(:@a, 'dog')
-  #   end
-
-  # end
 
 end
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,36 +4,27 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
-  # def show
-  #   @cocktail  = Cocktail.find(params[:id])
-  # end
-
   def new
     @cocktail  = Cocktail.new
   end
 
   def create
+
     @cocktail  = Cocktail.new(cocktail_params)
     if @cocktail.save
-      # no need for app/views/cocktails/create.html.erb
       redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-  def edit
-    @cocktail = Cocktail.find(params[:id])
-  end
-
   def update
-    @cocktail = Cocktail.find(params[:id])
+    super
     @cocktail.update(cocktail_params)
-    # Pas besoin de vue quand on fait un redirect_to
     redirect_to cocktail_path(@cocktail)
   end
 
-  private
+private
 
   def cocktail_params
     params.require(:cocktail).permit(
